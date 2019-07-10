e5cd677750ea4122347d815bba02518e70e27d97
heyLinsir <niuyl14@mails.tsinghua.edu.cn>
2019-07-10 15:42:41 +0800

    change code style

--- cotk/downloader.py
+++ cotk/downloader.py
@@ -15,8 +15,8 @@ def load_file_from_url(url, force=False, cache_dir=None):
 	Arguments:
 		url(str): A url indicating the file online.
 		force(bool): Force to download and ignore the existing file.
-		cache_dir(str, option): A path indicating where the cache place. If ``None``, a default cache path is
-			used.
+		cache_dir(str, option): A path indicating where the cache place. 
+		If ``None``, a default cache path is used.
 
 	Returns:
 		(str) The local path of downloaded model.
